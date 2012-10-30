ffecdc63afbd060dcbb2dfe86ee7f15ad38de20d
Frédéric Campo <strall@hotmail.fr>
2012-10-30 01:06:37 +0100

    Modifications readme, gitignore et ajout du fichier error manager

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Projet par Frédéric Campo
+Calculat0r.
 ==========================
 
-Calculat0r.
--------------
+Projet par Frédéric Campo
+-------------------------
 
 Reconnait dynamiquement les chaines de caractères tapée par l'utilisateur et effectue les calculs demandés. Non fonctionnel à l'heure actuelle.
---
+++ error_manager.cpp
@@ -0,0 +1,20 @@
+/************************************************
+*Auteur  : Frédéric Campo
+*Projet  : Calculatrice intelligente
+*Une calculatrice qui reconnait dynamiquement
+*les chaines de calculs tapés par l'utilisateur
+*Version : 0.2 (non fonctionnel)
+*TODO:
+*   - Opérations de base
+*   - Calculs d'équations a plusieurs variables
+*   - Calculs de complexes
+*   - Error manager
+*************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void error_manager();
